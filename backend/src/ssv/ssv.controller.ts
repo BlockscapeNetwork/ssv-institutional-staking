4f1aca95ca2e9fe237b0cb316717e552b3c58f0d
Norman Saade <norman.saade@gmail.com>
2022-12-06 19:50:20 -1000

    move required methods to ssv module

--- backend/src/ssv/ssv.controller.ts
+++ backend/src/ssv/ssv.controller.ts
@@ -1,18 +1,24 @@
-import { Controller, Get } from "@nestjs/common";
-import { ApiTags } from "@nestjs/swagger";
-import { SsvService } from "./ssv.service";
+import { Controller, Get, Post } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import { SsvService } from './ssv.service';
 
 @ApiTags('ssv')
 @Controller('api/v1/ssv')
 export class SsvController {
-  constructor(
-    private ssvService: SsvService,
-  ) {
-  }
+  constructor(private ssvService: SsvService) {}
 
   @Get('key-store')
   getKeyStore(): Promise<string> {
     return this.ssvService.getKeyStore();
   }
 
+  @Get('payload-register-validator')
+  getPayloadForRegisterValidator(): Promise<any> {
+    return this.ssvService.getPayloadForRegisterValidator();
+  }
+
+  @Post('register-validator')
+  registerValidator(): Promise<any> {
+    return this.ssvService.registerValidatorSSV();
+  }
 }
--- backend/src/ssv/ssv.module.ts
+++ backend/src/ssv/ssv.module.ts
@@ -1,13 +1,13 @@
-import { Module } from "@nestjs/common";
-import { TypeOrmModule } from "@nestjs/typeorm";
-import { Keystore } from "src/keystore/keystore.entity";
-import { KeystoreService } from "src/keystore/keystore.service";
-import { SsvController } from "./ssv.controller";
-import { SsvService } from "./ssv.service";
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Keystore } from 'src/keystore/keystore.entity';
+import { KeystoreService } from 'src/keystore/keystore.service';
+import { SsvController } from './ssv.controller';
+import { SsvService } from './ssv.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Keystore])],
   providers: [SsvService, KeystoreService],
   controllers: [SsvController],
 })
-export class SsvModule { }
+export class SsvModule {}
